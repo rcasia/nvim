a355daa0e620e886ad2a0650e60171855726fd18
Ricardo Casía <ricardocasia.dev@gmail.com>
2024-09-18 04:00:13 +0200

    nvim-spider plugin

---
+++ lua/plugins/motions.lua
@@ -0,0 +1,10 @@
+return {
+	{
+    "chrisgrieser/nvim-spider",
+    keys = {
+      { mode = { "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<cr>", desc = "spider-w" },
+      { mode = { "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<cr>", desc = "spider-e" },
+      { mode = { "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<cr>", desc = "spider-b" },
+    },
+  },
+}
