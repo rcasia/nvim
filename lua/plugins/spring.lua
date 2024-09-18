de6a7f851b52b474688025d2d067d90ea7ba8e1b
Ricardo Casía <ricardocasia.dev@gmail.com>
2024-09-18 04:02:42 +0200

    spring-boot plugin

---
+++ lua/plugins/spring.lua
@@ -0,0 +1,11 @@
+
+return {
+  {
+    "JavaHello/spring-boot.nvim",
+    ft = "java",
+    dependencies = {
+      "mfussenegger/nvim-jdtls", -- or nvim-java, nvim-lspconfig
+      "ibhagwan/fzf-lua", -- optional
+    },
+  }
+}
