aea147ce61d6bd1c9b4f6a954611d7acd8a7d3f2
Ricardo Casía <ricardocasia.dev@gmail.com>
2024-09-18 03:59:17 +0200

    savior.nvim

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,18 +1,8 @@
 return {
   {
-    "pocco81/auto-save.nvim",
-    config = function()
-      require("auto-save").setup({
-        write_all_buffers = true,
-        execution_message = {
-          message = function(bufnr)
-            return ""
-          end,
-          dim = 0.18, -- dim the color of `message`
-          cleaning_interval = 5000, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-        },
-        trigger_events = { "CursorHold" },
-      })
-    end,
-  },
+    "willothy/savior.nvim",
+    dependencies = { "j-hui/fidget.nvim" },
+    event = { "InsertEnter", "TextChanged" },
+    config = true
+  }
 }
