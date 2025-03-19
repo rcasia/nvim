c193f4527b79fc1daeb8e46ead6e275f2f0cfcc1
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-03-19 05:32:00 +0100

    to know when i enter recording mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -21,3 +21,24 @@ end
 
 -- execute
 -- autosave()
+
+--- TO KNOW WHEN A RECORDING IS BEING MADE
+local original_bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  callback = function()
+    local hl = vim.api.nvim_get_hl(0, { name = "Normal" })
+    original_bg = hl.bg or "NONE"
+    vim.api.nvim_set_hl(0, "Normal", { bg = "#222f3e", fg = hl.fg }) -- Cambia el fondo, manteniendo el color de texto
+  end,
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+  callback = function()
+    if original_bg and original_bg ~= "NONE" then
+      vim.api.nvim_set_hl(0, "Normal", { bg = string.format("#%06x", original_bg) }) -- Restaura el color original
+    else
+      vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" }) -- Restaura el fondo transparente si era necesario
+    end
+  end,
+})
