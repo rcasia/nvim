3b1c6cd03143a665a33e355d80de50d6afd2d8e2
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-04-21 21:40:57 +0200

    fix: update mason-lspconfig repo from williamboman to mason-org org

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -2,7 +2,7 @@ return {
   -- Pin mason-lspconfig to v1.x to maintain compatibility with LazyVim's
   -- use of the `mappings.server` module, which was removed in v2.x.
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     tag = "v1.32.0",
   },
 }
