460c3c733e619afb61405dabd755d7e5556be5a7
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-04-23 00:03:59 +0200

    fix: remove obsolete mason-lspconfig v1 pin breaking jdtls/LSP setup
    Current LazyVim requires mason-lspconfig v2 API (mappings.get_mason_map),
    so pinning to v1.32.0 prevented all servers (including jdtls) from being
    auto-installed and registered with lspconfig.

--- lua/plugins/mason-lspconfig.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- Pin mason-lspconfig to v1.x to maintain compatibility with LazyVim's
-  -- use of the `mappings.server` module, which was removed in v2.x.
-  {
-    "mason-org/mason-lspconfig.nvim",
-    tag = "v1.32.0",
-  },
-}
